9293142a2a62c3d68a3b08c28c757603a779b1bd
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-10-12 11:25:06 +0300

    add component UseEffect.tsx

--- src/UseEffect.tsx
+++ src/UseEffect.tsx
@@ -1,31 +1,60 @@
-import React, {useEffect, useState } from "react"
+import React, {useEffect, useState} from "react"
 
 
-export const SimpleExample =() => {
-    const [fake, setFake]=useState(1)
-    const [counter, setCounter] = useState(1)
+// export const SimpleExample =() => {
+//     const [fake, setFake]=useState(1)
+//     const [counter, setCounter] = useState(1)
+//
+//
+//
+//     useEffect(()=> {
+//         console.log('useEffect every render')
+//         document.title=counter.toString();
+//     })
+//     useEffect(()=> {
+//         console.log('useEffect only first render(componentDidMount)')
+//         document.title=counter.toString();
+//     },[])
+//     useEffect(()=> {
+//         console.log('useEffect first render and always when change counter')
+//         document.title=counter.toString();
+//     }, [counter])
+//
+//
+//     return <>
+//         Hello, {counter}
+//         <button onClick={() => setCounter(counter+1)}>+++</button>
+//         {fake}
+//         <button onClick={() => setFake(fake+1)}>Fake+</button>
+//
+//     </>
+// }
 
 
+export const SetTimeOutExample = () => {
+    const [timerId, setTimerId] = useState<number>()
+    const [date, setDate] = useState<Date>()
 
-    useEffect(()=> {
-        console.log('useEffect every render')
-        document.title=counter.toString();
-    })
-    useEffect(()=> {
-        console.log('useEffect only first render(componentDidMount)')
-        document.title=counter.toString();
-    },[])
-    useEffect(()=> {
-        console.log('useEffect first render and always when change counter')
-        document.title=counter.toString();
-    }, [counter])
+    function stopTime(){
+        clearInterval(timerId)
+    }
 
+    console.log('SetTimeOutExample')
+    useEffect(() => {
+       const id:number = +setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+        setTimerId(id)
+    }, [])
+
+    const stringTime = date?.toLocaleTimeString()
 
     return <>
-        Hello, {counter}
-        <button onClick={() => setCounter(counter+1)}>+++</button>
-        {fake}
-        <button onClick={() => setFake(fake+1)}>Fake+</button>
+        <div>Hello, time: {stringTime}</div>
+        <button onClick={stopTime}>STOP TIME</button>
+        {/*<button onClick={() => setCounter(counter + 1)}>+++</button>*/}
+        {/*<div> fake: {fake} </div>*/}
+        {/*<button onClick={() => setFake(fake + 1)}>Fake+</button>*/}
 
     </>
 }
