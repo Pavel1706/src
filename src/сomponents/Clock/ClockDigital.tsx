1aec015d6e482514835307b009861d701b109aa5
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-10-24 00:49:38 +0300

    add ClockDigital.tsx

--- src/сomponents/Clock/ClockDigital.tsx
+++ src/сomponents/Clock/ClockDigital.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import { useEffect } from "react"
+import style from './clock.module.css';
+import {ClockAnalog} from "./AnalogClock";
 
 
-
-export const Clock = () => {
+export const ClockDigital = () => {
     const [timerId, setTimerId] = useState<number>()
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
 
     function stopTime(){
         clearInterval(timerId)
@@ -18,14 +19,21 @@ export const Clock = () => {
                 setDate(new Date())
             }, 1000)
             setTimerId(id)
+            return ()=> {
+                debugger
+                clearInterval(id)
+            }
         }, [])
 
 
     const stringTime = date?.toLocaleTimeString()
 
+
     return <>
-        <div>Hello, time: {stringTime}</div>
+        <ClockAnalog date={date} />
+        {/*<div>Hello, time: {stringTime}</div>*/}
         {/*<button onClick={StarTime}>START TIME</button>*/}
+        <div className={style.clock__hand}>{stringTime}</div>
         <button onClick={stopTime}>STOP TIME</button>
         {/*<button onClick={() => setCounter(counter + 1)}>+++</button>*/}
         {/*<div> fake: {fake} </div>*/}
